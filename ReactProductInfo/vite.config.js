7a6cdda6651c1e8b6f80dd12926ef1f091cdaf08
[ABDUL QADIR] <159106895+Abdulqadir5d@users.noreply.github.com>
2025-10-27 01:56:28 +0500

    second

--- ReactProductInfo/vite.config.js
+++ ReactProductInfo/vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Set the base path to your repository name
+  base: "/React-Assigment-1/", 
   plugins: [react()],
 })
